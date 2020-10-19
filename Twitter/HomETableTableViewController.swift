01e6fa3abdb0567da172728f1b902534d63e8f17
jttran28 <jttran2019@gmail.com>
2020-10-19 05:33:07 -0400

    unit 4

--- Twitter/HomETableTableViewController.swift
+++ Twitter/HomETableTableViewController.swift
@@ -12,16 +12,45 @@ class HomETableTableViewController: UITableViewController {
     
     var tweetArray = [NSDictionary]()
     var numberOfTweet: Int!
+    let myRefreshControl = UIRefreshControl()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //loadTweet()
+        myRefreshControl.addTarget(self, action: #selector(loadTweet), for: .valueChanged)
+        tableView.refreshControl = myRefreshControl
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.estimatedRowHeight = 150
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         loadTweet()
     }
     
-    func loadTweet(){
-        
+    @objc func loadTweet(){
+        numberOfTweet = 20
         let myURL = "https://api.twitter.com/1.1/statuses/home_timeline.json"
-        let myParams = ["count": 10]
+        let myParams = ["count": numberOfTweet]
+        TwitterAPICaller.client?.getDictionariesRequest(url: myURL, parameters: myParams, success: { (tweets: [NSDictionary]) in
+            self.tweetArray.removeAll()
+            for tweet in tweets {
+                self.tweetArray.append(tweet)
+            }
+            
+            self.tableView.reloadData()
+            self.myRefreshControl.endRefreshing()
+            
+        }, failure: { (Error) in
+            print("Could not retreive tweets")
+        })
+        
+    }
+    
+    func loadMoreTweets(){
+        let myURL = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+        numberOfTweet = numberOfTweet + 20
+        let myParams = ["count": numberOfTweet]
         TwitterAPICaller.client?.getDictionariesRequest(url: myURL, parameters: myParams, success: { (tweets: [NSDictionary]) in
             self.tweetArray.removeAll()
             for tweet in tweets {
@@ -34,6 +63,12 @@ class HomETableTableViewController: UITableViewController {
             print("Could not retreive tweets")
         })
         
+    }
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath){
+        if indexPath.row + 1 == tweetArray.count {
+            loadMoreTweets()
+        }
     }
 
     @IBAction func onLogout(_ sender: Any) {
@@ -56,6 +91,10 @@ class HomETableTableViewController: UITableViewController {
         if let imageData = data {
             cell.profileImageView.image = UIImage(data: imageData)
         }
+        
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
         
         return cell
     }
--- Twitter/TweetCell.swift
+++ Twitter/TweetCell.swift
@@ -9,22 +9,75 @@
 import UIKit
 
 class TweetCell: UITableViewCell {
-
+    
+    var favorite:Bool = false
+    var tweetId:Int = -1
+    //var retweeted:Bool = false
+    
     @IBOutlet weak var profileImageView: UIImageView!
     
     @IBOutlet weak var userNameLabel: UILabel!
     
     @IBOutlet weak var tweetContent: UILabel!
     
+    @IBOutlet weak var retweenButton: UIButton!
+    
+    @IBOutlet weak var favButton: UIButton!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
     }
-
+    
+    func setRetweeted(_ isRetweeted:Bool){
+        if(isRetweeted){
+            retweenButton.setImage(UIImage(named: "retweet-icon-green"), for: UIControl.State.normal)
+            retweenButton.isEnabled = false
+        } else {
+            retweenButton.setImage(UIImage(named: "retweet-icon"), for: UIControl.State.normal)
+            retweenButton.isEnabled = true
+        }
+    }
+    
+    func setFavorite(_ isFavorited:Bool){
+        favorite = isFavorited
+        if(favorite){
+            favButton.setImage(UIImage(named: "favor-icon-red"), for: UIControl.State.normal)
+        } else {
+            favButton.setImage(UIImage(named: "favor-icon"), for: UIControl.State.normal)
+        }
+    }
+    
+    @IBAction func favoriteTweet(_ sender: Any) {
+        let tobeFavorited = !favorite
+        if (tobeFavorited) {
+            TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(true)
+            }, failure: { (error) in
+                print("Favorite did not succeed: \(error)")
+            })
+        } else {
+            TwitterAPICaller.client?.unfavoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(false)
+            }, failure: { (error) in
+                print("Unfavorite did not succeed: \(error)")
+            })
+        }
+    }
+    
+    @IBAction func retweet(_ sender: Any) {
+        TwitterAPICaller.client?.retweet(tweetId: tweetId, success: {
+            self.setRetweeted(true)
+        }, failure: { (error) in
+            print("Error in retweeting: \(error)")
+        })
+        
+    }
+    
 }
